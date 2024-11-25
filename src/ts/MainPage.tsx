c120f6f0464b349bfebeddcd4368d1a528cb1aca
Санатов Ильдар Русланович <SanatovIR@MILKOM-KOMOS.RU>
2024-11-25 16:52:15 +0400

    Добавляю верстку и функциональность для страницы авторизации /auth

--- src/ts/MainPage.tsx
+++ src/ts/MainPage.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 export default function MainPage({pass}: {pass:string | null}) {
     return(
-        <div className="w-full h-full">
+        <div className="w-full h-full flex justify-center items-center">
             <Header pass ={pass}/>
         </div>
     )
